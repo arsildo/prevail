e96b3efcecaae084fed6aa3095276e7c9560ec30
arsildo <arsildo.murati@gmail.com>
2023-01-25 18:05:35 +0100

    added countryCodeToEmoji logic

--- app/src/main/java/com/arsildo/prevail/utils/CountriesISOFlags.kt
+++ app/src/main/java/com/arsildo/prevail/utils/CountriesISOFlags.kt
@@ -1,22 +1,11 @@
 package com.arsildo.prevail.utils
 
-fun getCountryFromCode(country: String) = when (country) {
-    "AC" -> "\uD83C\uDDE6\uD83C\uDDE8"
-    "AD" -> "\uD83C\uDDE6\uD83C\uDDE9"
-    "AE" -> "\uD83C\uDDE6\uD83C\uDDEA"
-    "AF" -> "\uD83C\uDDE6\uD83C\uDDEB"
-    "AG" -> "\uD83C\uDDE6\uD83C\uDDEC"
-    "AI" -> "\uD83C\uDDE6\uD83C\uDDEE"
-    "AL" -> "\uD83C\uDDE6\uD83C\uDDF1"
-    "AM" -> "\uD83C\uDDE6\uD83C\uDDF2"
-    "AO" -> "\uD83C\uDDE6\uD83C\uDDF4"
-    "AQ" -> "\uD83C\uDDE6\uD83C\uDDF6"
-    "AR" -> "\uD83C\uDDE6\uD83C\uDDF7"
-    "AS" -> "\uD83C\uDDE6\uD83C\uDDF8"
-    "AT" -> "\uD83C\uDDE6\uD83C\uDDF9"
-    "AU" -> "\uD83C\uDDE6\uD83C\uDDFA"
-    "AW" -> "\uD83C\uDDE6\uD83C\uDDFC"
-    "AX" -> "\uD83C\uDDE6\uD83C\uDDFD"
-    "AZ" -> "\uD83C\uDDE6\uD83C\uDDFF"
-    else -> ""
+import java.util.Locale
+
+fun countryCodeToEmoji(countryCode: String): String {
+    return countryCode
+        .uppercase(Locale.US)
+        .map { char -> Character.codePointAt("$char", 0) - 0x41 + 0x1F1E6 }
+        .map { codePoint -> Character.toChars(codePoint) }
+        .joinToString(separator = "") { charArray -> String(charArray) }
 }
